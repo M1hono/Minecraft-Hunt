8a5b5256c20ec9938a28a7f7dc49fb47b0a70bce
m1hono <2311374931@qq.com>
2024-06-16 10:09:21 +1000

    Optimize and refactor the structure again.

--- kubejs/server_scripts/Develop/InfoLogger.js
+++ kubejs/server_scripts/Develop/InfoLogger.js
@@ -3,6 +3,7 @@
  * @description This script prints various information from your modpack to the console.
  * 
  * Available commands:
+ * - 'GetRecipe': Prints all recipes.
  * - 'GetDamage': Prints all damage types.
  * - 'GetAttribute': Prints all attributes.
  * - 'GetEnchantment': Prints all enchantments.
@@ -12,19 +13,17 @@
  * - 'ListRegistries': Lists all available registries.
  */
 PlayerEvents.chat(event => {
-
     const { message, level } = event;
 
-    // Get Registry Key of ResourceKey through Reflection.
+    // Load necessary classes
     const $ResourceKey = Java.loadClass("net.minecraft.resources.ResourceKey");
     const $TierSortingRegistry = Java.loadClass("net.minecraftforge.common.TierSortingRegistry");
 
     // Utility function to create registry keys
-    function createRegistryKey(name) {
-        return $ResourceKey.createRegistryKey(name);
-    }
+    const createRegistryKey = (name) => $ResourceKey.createRegistryKey(name);
 
     // Create registry keys for the resources we are interested in
+    const RECIPE = createRegistryKey("recipe_type");
     const DAMAGE_TYPE = createRegistryKey("damage_type");
     const ATTRIBUTE = createRegistryKey("attribute");
     const ENCHANTMENT = createRegistryKey("enchantment");
@@ -36,63 +35,43 @@ PlayerEvents.chat(event => {
      * Prints all entries from a given registry key to the console.
      * @param {ResourceKey} registryKey - The registry key to get entries from.
      */
-    function printRegistryEntries(registryKey) {
-        let registry = level.registryAccess().registryOrThrow(registryKey);
-        let entries = registry.entrySet();
-        let entrySet = new Set();
+    const printRegistryEntries = (registryKey) => {
+        const registry = level.registryAccess().registryOrThrow(registryKey);
+        const entries = registry.entrySet();
+        const entrySet = new Set();
 
-        // Get all entries and add them to the set.
-        for (let entry of entries) {
-            let resourceKey = entry.getKey();
-            entrySet.add(resourceKey.location().toString());
-        }
+        // Get all entries and add them to the set
+        entries.forEach(entry => entrySet.add(entry.getKey().location().toString()));
 
-        // Print all entries to the console.
-        let allEntries = '\n' + Array.from(entrySet).join('\n');
-        console.info(allEntries);
-    }
+        // Print all entries to the console
+        console.info('\n' + Array.from(entrySet).join('\n'));
+    };
 
     /**
      * Prints all tier names to the console.
      */
-    function printTiers() {
-        let tiers = $TierSortingRegistry.getSortedTiers();
-        let tierNames = tiers.map(tier => $TierSortingRegistry.getName(tier).toString());
+    const printTiers = () => {
+        const tiers = $TierSortingRegistry.getSortedTiers();
+        const tierNames = tiers.map(tier => $TierSortingRegistry.getName(tier).toString());
 
-        // Print all tier names to the console.
-        let allTiers = '\n' + tierNames.join('\n');
-        console.info(allTiers);
-    }
+        // Print all tier names to the console
+        console.info('\n' + tierNames.join('\n'));
+    };
 
-    /** 
-     * Handle different chat messages to trigger the corresponding actions.
-     * @param {string} message - The chat message to trigger the event.
-     */
-    if (message === 'GetDamage') {
-        printRegistryEntries(DAMAGE_TYPE);
-    }
+    // Mapping of commands to their respective actions
+    const commandActions = {
+        'GetRecipe': () => printRegistryEntries(RECIPE),
+        'GetDamage': () => printRegistryEntries(DAMAGE_TYPE),
+        'GetAttribute': () => printRegistryEntries(ATTRIBUTE),
+        'GetEnchantment': () => printRegistryEntries(ENCHANTMENT),
+        'GetBiome': () => printRegistryEntries(BIOME),
+        'GetFluid': () => printRegistryEntries(FLUID),
+        'GetEffect': () => printRegistryEntries(EFFECT),
+        'GetTier': () => printTiers()
+    };
 
-    if (message === 'GetAttribute') {
-        printRegistryEntries(ATTRIBUTE);
-    }
-
-    if (message === 'GetEnchantment') {
-        printRegistryEntries(ENCHANTMENT);
-    }
-
-    if (message === 'GetBiome') {
-        printRegistryEntries(BIOME);
-    }
-
-    if (message === 'GetFluid') {
-        printRegistryEntries(FLUID);
-    }
-
-    if (message === 'GetEffect') {
-        printRegistryEntries(EFFECT);
-    }
-
-    if (message === 'GetTier') {
-        printTiers();
+    // Execute the appropriate action based on the chat message
+    if (commandActions[message]) {
+        commandActions[message]();
     }
 });
